cbc28fe806121a4635648295ae2381c4e9a5cd2e
Jenna C <jennac0312@gmail.com>
2023-08-14 10:10:07 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express()
+
+const PORT = 3000
+
+// MIDDLEWARE
+
+
+
+// ROUTES
+app.get('/pokemon', ( req, res ) => {
+    
+})
+
+
+
+
+
+app.listen(PORT, ( req, res ) => {
+    console.log(`Server is running on port ${PORT}`)
+})
