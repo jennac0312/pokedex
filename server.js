848059ef7dd98899a2d69537c0b3d3fb3ab491d8
Jenna C <jennac0312@gmail.com>
2023-08-18 12:17:40 -0400

    mongoose schema created. seed route functioning

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const app = express()
-
 const PORT = 3000
 
-const pokemons = require('./models/pokemon')
+require("dotenv").config() 
+const mongoose = require("mongoose")
+const MONGO_URI = process.env.MONGO_URI
+
+const Pokemon = require('./models/Pokemon') // schema
+const pokemons = require('./utilities/pokemon') // for seed
 
 // MIDDLEWARE
 // view engine
@@ -13,11 +17,17 @@ app.engine("jsx", require("express-react-views").createEngine())
 // runs between all routes
 app.use((req, res, next) => {
     console.log('I run for all routes');
-    next();
+    next(); // calls next step
 });
 
 // tell express to use the middleware.. view body of a post request
 app.use(express.urlencoded({extended:false}));
+
+// connect to DB
+mongoose.connect( MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 
 
@@ -25,19 +35,37 @@ app.use(express.urlencoded({extended:false}));
 app.get('/', ( req, res ) => {
     res.send(`<h1>Welcome to the Pokemon App!</h1>`)
 })
-app.get('/pokemon', ( req, res ) => {
-    // res.send(pokemons)
-    res.render("Index", { pokemons : pokemons }) // dont like the plural but...
+
+// SEED
+app.get('/pokemon/seed',async(req,res)=>{
+    //Deleting All Current Data(optional)
+    await Pokemon.deleteMany({}) //PokemonSchema
+    //create a list of pokemon
+    await Pokemon.create(pokemons) //imported pokemon data array
+    res.redirect('/pokemon')
 })
 
-app.get('/pokemon/new', (  req, res ) => {
+app.get('/pokemon', async ( req, res ) => {
+    // res.send(pokemons)
+
+    try {
+        const allPokemon = await Pokemon.find({})
+        res.render("Index", { pokemons : allPokemon }) // dont like the plural but...
+    } catch (error) {
+        res.status(500).send( "Server Error" )
+    }
+})
+
+app.get('/pokemon/new', ( req, res ) => {
     res.render("New")
 })
+
+
 
 // route matches path from FORM ACTION ATTRIBUTE
 app.post('/pokemon', ( req, res ) => {
     // const newPokemon = await req.body
-
+    
     // res.send(newPokemon)
     console.log( req.body )
     pokemons.push( req.body )
@@ -49,15 +77,23 @@ app.post('/pokemon', ( req, res ) => {
 
 app.get('/pokemon/:id', ( req, res ) => {
     // res.send( req.params.id )
-
+    
     let { id } = req.params
     res.render("Show", { pokemon : pokemons[id] })
 })
 
 
 
+// API
+const fetchPokemon = async (name) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/${name}`)
+    console.log(response)
+}
 
 
-app.listen(PORT, ( req, res ) => {
-    console.log(`Server is running on port ${PORT}`)
+mongoose.connection.once( 'open', () => {
+    console.log('Connected to database')
+    app.listen(PORT, ( req, res ) => {
+        console.log(`Server is running on port ${PORT}`)
+    })
 })
--- views/Index.jsx
+++ views/Index.jsx
@@ -4,8 +4,20 @@ const Index = ( { pokemons } ) => {
 
     const myStyle = {
         color: '#ffffff',
-        backgroundColor: '#000000',
+        backgroundColor: 'skyblue',
+        fontSize: "2em",
     };
+
+    const linkStyle = {
+        textDecoration: "none",
+        color: 'slategrey',
+        fontWeight: "bold",
+        listStyle: "none",
+        margin: '10px',
+        display: 'flex',
+        alignItems: "center",
+        justifyContent: "left"
+    }
 
   return (
     <div style={myStyle}>
@@ -14,10 +26,21 @@ const Index = ( { pokemons } ) => {
 
 
       <ul>
-        { pokemons.map((pokemon, index) => {
+        { pokemons?.map((pokemon, index) => {
+            // pokemon.name.split('').forEach(( letter, index ) => {
+            //     index === 0 && letter.toUpperCase
+            // })
+            let firstLetter = pokemon?.name[0].toUpperCase()
+            let rest = pokemon?.name.substring(1)
+
+
             return (
-                <a href={`/pokemon/${index}`} key={index}>
-                    <li>{pokemon.name}</li>
+                <a href={`/pokemon/${index}`} key={index} style={ linkStyle }>
+                    <li>
+                        <img src="https://www.freeiconspng.com/thumbs/pokeball-png/pokeball-transparent-png-2.png" alt="pokeball" style={{ height: '30px', marginRight: "10px" }} />
+                        <span>{ pokemon?.name.charAt(0).toUpperCase() + rest }</span>
+                        
+                    </li>
                 </a>
             )
         }) }
